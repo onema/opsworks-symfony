ad9a6d9035d5ffa8c31b08c2a194674445a1fa52
Juan Manuel Torres <kinojman@gmail.com>
2015-06-13 11:17:55 -0700

    Update metadata license

--- symfony/metadata.rb
+++ symfony/metadata.rb
@@ -1,11 +1,10 @@
 name              "symfony"
 maintainer        "Juan Manuel Torres"
 maintainer_email  "kinojman@gmail.com"
-license           "Apache 2"
+license           "Apache License v2"
 description       "Set of recipes to help setup and deploy Symfony 2 applications."
 version           "1.0.0"
 
-depends "composer"
-
-recipe "composer::install", "Installs composer packages."
-recipe "composer::update", "Update composer packages."
+depends           "composer"
+recipe            "composer::install", "Installs composer packages."
+recipe            "composer::update", "Update composer packages."
